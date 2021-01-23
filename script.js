989c49f24e86e0dda811c94c7c4d7a3e18584c90
chrisberanek <74572231+chrisberanek@users.noreply.github.com>
2021-01-23 14:19:33 -0600

    Page Layout

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ let startButton = document.querySelector('.js-start');
 let info = document.querySelector('.js-info');
 let heading = document.querySelector('.js-heading');
 let tileContainer = document.querySelector('.js-container');
+
+function resetGame(text) {
+    alert(text);
+    computerSequence = [];
+    playerSequence = [];
+    level = 0;
+    startButton.classList.remove('hidden');
+    heading.textContent = 'Play Again';
+    info.classList.add('hidden');
+    tileContainer.classList.add('unclickable');
+  }
+
+
+
+function startGame() {
+  startButton.classList.add('hidden');
+  info.classList.remove('hidden');
+  info.textContent = 'Watch Computer Sequence';
+}
